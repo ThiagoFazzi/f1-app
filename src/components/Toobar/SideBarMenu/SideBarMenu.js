6d38326e226f907f85f7a9ffb663b4bb035ba90b
ThiagoFazzi <thiago@xtwo.com.br>
2019-01-29 15:47:33 +0100

    created and implemented backdrop for mobile devices, fixed SideBarMenu top allowing to show hamburger icon and logo

--- src/components/Toobar/SideBarMenu/SideBarMenu.js
+++ src/components/Toobar/SideBarMenu/SideBarMenu.js
@@ -1,7 +1,7 @@
 import React from'react'
 import classes from './SideBarMenu.module.css'
 import Menu from '../Menu/Menu'
-//import Backdrop from '../../UI/Backdrop/Backdrop'
+import Backdrop from '../Backdrop/Backdrop'
  
 const sideBarMenu = (props) => {
   let attachedClasses = [classes.SideBarMenu, classes.Close]
@@ -10,7 +10,7 @@ const sideBarMenu = (props) => {
   }
   return(
     <>
-      {/*<Backdrop show={props.open} clicked={props.closed}/>*/}
+      <Backdrop show={props.open} clicked={props.closed}/>
       <div className={attachedClasses.join(' ')}>
         <nav>
           <Menu />
