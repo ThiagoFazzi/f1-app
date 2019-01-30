49744c6819b8330448dc28cf0b1d7d07d8dbc9fd
ThiagoFazzi <thiago@xtwo.com.br>
2019-01-30 20:13:24 +0100

    Updates

---
+++ precache-manifest.488849c1810b5a5c45170844d0e3d4d9.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "27ed428270db6db40ebf4f802192b8bb",
+    "url": "/f1-app/static/media/lotus_racing.27ed4282.jpg"
+  },
+  {
+    "revision": "e4aa826347a4f4802d3f",
+    "url": "/f1-app/static/css/main.83ff7d52.chunk.css"
+  },
+  {
+    "revision": "5b497020a70d1aa168bb",
+    "url": "/f1-app/static/js/1.5b497020.chunk.js"
+  },
+  {
+    "revision": "4282cc0c3fa9a5b38b1a",
+    "url": "/f1-app/static/js/runtime~main.4282cc0c.js"
+  },
+  {
+    "revision": "d3cff7684a9535d1a4d62b53a2265dee",
+    "url": "/f1-app/static/media/f1-logo.d3cff768.png"
+  },
+  {
+    "revision": "a47b14b3c865a126b739c811510fe380",
+    "url": "/f1-app/static/media/bmw_sauber.a47b14b3.jpg"
+  },
+  {
+    "revision": "8bbfe5eeaccfc4fba3d1a0cb779ab60e",
+    "url": "/f1-app/static/media/caterham.8bbfe5ee.jpg"
+  },
+  {
+    "revision": "b091a3d2e203b9270bb24be7bbc125d6",
+    "url": "/f1-app/static/media/ferrari.b091a3d2.jpg"
+  },
+  {
+    "revision": "8b43ad453de3b398ec4579008da548e3",
+    "url": "/f1-app/static/media/force_india.8b43ad45.jpg"
+  },
+  {
+    "revision": "894db883b1803a3bd7d46babb59b10aa",
+    "url": "/f1-app/static/media/haas.894db883.jpg"
+  },
+  {
+    "revision": "727503ba5c9349875d9269ac9e447e0b",
+    "url": "/f1-app/static/media/hrt.727503ba.jpg"
+  },
+  {
+    "revision": "66624c52a52286d6b5befc6ccbcaeb21",
+    "url": "/f1-app/static/media/lotus_f1.66624c52.jpg"
+  },
+  {
+    "revision": "e4aa826347a4f4802d3f",
+    "url": "/f1-app/static/js/main.e4aa8263.chunk.js"
+  },
+  {
+    "revision": "4ce20d3c66080fe73dbed70e2fc60db8",
+    "url": "/f1-app/static/media/manor.4ce20d3c.jpg"
+  },
+  {
+    "revision": "72451cc9c9be636b0a0ca7d9920b0362",
+    "url": "/f1-app/static/media/marussia.72451cc9.jpg"
+  },
+  {
+    "revision": "deb8f73797a429fe13741d78f27693f8",
+    "url": "/f1-app/static/media/mclaren.deb8f737.jpg"
+  },
+  {
+    "revision": "9f6208624242a4c3ce726762bbc5c195",
+    "url": "/f1-app/static/media/mercedes.9f620862.jpg"
+  },
+  {
+    "revision": "40ee0a2eabb50c0ad63e0ef9715054a2",
+    "url": "/f1-app/static/media/red_bull.40ee0a2e.jpg"
+  },
+  {
+    "revision": "21bd8a886f3d479e81fe5af0517213ee",
+    "url": "/f1-app/static/media/renault.21bd8a88.jpg"
+  },
+  {
+    "revision": "c6f487bc22e810362cd8dbef3b7844de",
+    "url": "/f1-app/static/media/sauber.c6f487bc.jpg"
+  },
+  {
+    "revision": "7a9903bce10b2ea9dcda2c6892d415e1",
+    "url": "/f1-app/static/media/toro_rosso.7a9903bc.jpg"
+  },
+  {
+    "revision": "be04072768ca6fba87d061e083c7c171",
+    "url": "/f1-app/static/media/toyota.be040727.jpg"
+  },
+  {
+    "revision": "6efc752a74b8ae6ccc6449fbfcb88946",
+    "url": "/f1-app/static/media/virgin.6efc752a.jpg"
+  },
+  {
+    "revision": "042183214d33cfef31df35afecd5ec4c",
+    "url": "/f1-app/static/media/williams.04218321.jpg"
+  },
+  {
+    "revision": "ae6b6ec63b6e229bc646b21b557ff0ed",
+    "url": "/f1-app/index.html"
+  }
+];
